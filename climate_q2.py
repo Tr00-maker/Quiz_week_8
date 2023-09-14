f07f4dec38da9ffb05c812a84c2f07672662518f
Travis <travis.ralph@griffithuni.edu.au>
2023-09-14 23:55:43 +1000

    CSV Connection Created, imported into lists

--- climate_q2.py
+++ climate_q2.py
@@ -1,9 +1,20 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 
-years = []
-co2 = []
-temp = []
+#get current directory to avoid bugs
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+#construct full path to the file
+csv_file_path = os.path.join(current_dir, "climate.csv")
+
+#load data from the CSV file into a dataframe
+df = pd.read_csv(csv_file_path)
+
+#extract data to columns
+years = df['Year']
+co2 = df['CO2']
+temp = df['Temperature']
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
